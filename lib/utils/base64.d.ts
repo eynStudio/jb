7f9f67220d3aa96566deb242d6fac8bff77d36ef
pswang <pengshengwang@gmail.com>
2017-06-21 22:42:54 +0800

    add base64

---
+++ lib/utils/base64.d.ts
@@ -0,0 +1,10 @@
+export declare const utf8_encode: (string: any) => string;
+export declare const utf8_decode: (utftext: any) => string;
+export declare const encode: (input: any) => string;
+export declare const decode: (input: any) => string;
+export declare const base64: {
+    encode: (input: any) => string;
+    decode: (input: any) => string;
+    utf8_encode: (string: any) => string;
+    utf8_decode: (utftext: any) => string;
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 import {url} from './url'
 export {url}
+import {base64} from './base64'
+export {base64}
